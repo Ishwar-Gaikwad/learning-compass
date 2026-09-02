7928e7ac20ba736b9813b2b3d2b2ddda89fcba83
Ishwar Gaikwad <i.s.gaikwad87@gmail.com>
2026-09-02 17:49:08 +0530

    Fix backend production entry point

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
-import { validateEnv, config } from './src/config/env.config.js';
-import app from './src/app.js';
-import { connectDB } from './src/config/db.js';
+// import { validateEnv, config } from './config/env.config.js';
+// import app from './app.js';
+// import { connectDB } from './config/db.js';
+
+import { validateEnv, config } from './config/env.config.js';
+import app from './app.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 validateEnv();
